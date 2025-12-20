855b5eb043e7328ff587077f11eb1e1d7cc0eb3e
bala-amypo <bala@amypo.in>
2025-12-20 15:58:45 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,19 +1,20 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Employee CRUD API")
-                        .version("1.0")
-                        .description("Spring Boot CRUD with Swagger"));
-    }
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9417.pro604cr.amypo.ai/")
+                ));
+        }
 }
