09a5a96483d791570b7c4899538bed91a88768c9
bala-amypo <bala@amypo.in>
2025-12-22 18:19:40 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/EmployeeSkillRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.EmployeeSkill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeSkillRepository extends JpaRepository<EmployeeSkill, Long> {
+}
