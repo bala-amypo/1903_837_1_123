f3bc88cb1ed2306b76d32ae21a72c8f79d3b31ba
bala-amypo <bala@amypo.in>
2025-12-25 07:25:47 +0000

    User commit

--- src/main/java/com/example/demo/controller/SkillController.java
+++ src/main/java/com/example/demo/controller/SkillController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.Skill;
+import com.example.demo.model.Skill;
 import com.example.demo.service.SkillService;
 import org.springframework.web.bind.annotation.*;
 
