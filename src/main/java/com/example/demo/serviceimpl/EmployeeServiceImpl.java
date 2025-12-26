3f328ca39654a7699a36d76225cad57a6544de05
bala-amypo <bala@amypo.in>
2025-12-26 08:22:54 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/EmployeeServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/EmployeeServiceImpl.java
@@ -1,14 +1,12 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepository;
 import com.example.demo.service.EmployeeService;
-import com.example.demo.exception.ResourceNotFoundException;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeRepository repo;
@@ -17,26 +15,33 @@ public class EmployeeServiceImpl implements EmployeeService {
         this.repo = repo;
     }
 
+    @Override
     public Employee createEmployee(Employee e) {
         return repo.save(e);
     }
 
-    public Employee updateEmployee(Long id, Employee e) {
-        Employee existing = getEmployeeById(id);
-        existing.setFullName(e.getFullName());
-        existing.setEmail(e.getEmail());
-        return repo.save(existing);
+    @Override
+    public Employee updateEmployee(Long id, Employee update) {
+        Employee e = repo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
+
+        e.setFullName(update.getFullName());
+        e.setEmail(update.getEmail());
+        return repo.save(e);
     }
 
+    @Override
     public Employee getEmployeeById(Long id) {
         return repo.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
     }
 
+    @Override
     public List<Employee> getAllEmployees() {
         return repo.findAll();
     }
 
+    @Override
     public void deactivateEmployee(Long id) {
         Employee e = getEmployeeById(id);
         e.setActive(false);
