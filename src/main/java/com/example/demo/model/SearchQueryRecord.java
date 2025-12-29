055ef4bc9f2e70045c106e5161111fbfb2502662
bala-amypo <bala@amypo.in>
2025-12-29 09:54:57 +0000

    User commit

--- src/main/java/com/example/demo/model/SearchQueryRecord.java
+++ src/main/java/com/example/demo/model/SearchQueryRecord.java
@@ -21,6 +21,10 @@ public class SearchQueryRecord {
         if (resultsCount == null) resultsCount = 0;
     }
 
+    public void setId(Long id) {
+    this.id = id;
+}
+
     public Long getId() { return id; }
 
     public Long getSearcherId() { return searcherId; }
